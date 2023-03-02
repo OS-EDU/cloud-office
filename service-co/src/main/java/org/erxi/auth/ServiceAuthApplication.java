75fa3193b17b2eab597e53d4540f8a8cb9160a69
QUakeWang <1677980708@qq.ocm>
2023-03-02 17:31:11 +0800

    the CRUD of SysRoleController

--- service-co/src/main/java/org/erxi/auth/ServiceAuthApplication.java
+++ service-co/src/main/java/org/erxi/auth/ServiceAuthApplication.java
@@ -2,8 +2,10 @@ package org.erxi.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("org.erxi")
 public class ServiceAuthApplication {
 
     public static void main(String[] args) {
--- service-co/src/main/java/org/erxi/auth/controller/SysRoleController.java
+++ service-co/src/main/java/org/erxi/auth/controller/SysRoleController.java
@@ -1,14 +1,16 @@
 package org.erxi.auth.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.erxi.auth.service.SysRoleService;
 import org.erxi.common.Result;
 import org.erxi.model.system.SysRole;
+import org.erxi.vo.system.SysRoleQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +27,79 @@ public class SysRoleController {
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
         return Result.ok(roleList);
+    }
+
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page, /* 当前页 */
+                                @PathVariable Long limit, /* 每页显示记录数 */
+                                SysRoleQueryVo sysRoleQueryVo /* 条件对象 */) {
+        // 调用 service 中的方法实现
+        // 1、创建 Page 对象，传递分页相关参数
+        Page<SysRole> pageParam = new Page<>(page, limit);
+
+        // 2、封装条件，判断条件是否为空
+        LambdaQueryWrapper<SysRole> queryWrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if (!StringUtils.isEmpty(roleName)) {
+            // 封装 like 模糊查询
+            queryWrapper.like(SysRole::getRoleName, roleName);
+        }
+
+        // 3、调用方法实现
+        Page<SysRole> pageModel = sysRoleService.page(pageParam, queryWrapper);
+
+        return Result.ok(pageModel);
+    }
+
+    @ApiOperation("添加角色")
+    @PostMapping("save")
+    public Result save(@RequestBody SysRole role) {
+        boolean is_success = sysRoleService.save(role);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id查询")
+    @GetMapping("get/{id}")
+    public Result getById(@PathVariable Long id) {
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.ok(sysRole);
+    }
+
+    @ApiOperation("修改角色")
+    @PutMapping("update")
+    public Result update(@RequestBody SysRole role) {
+        boolean is_success = sysRoleService.updateById(role);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id删除")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        boolean is_success = sysRoleService.removeById(id);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("批量删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        boolean is_success = sysRoleService.removeByIds(idList);
+        if (is_success) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
 }
